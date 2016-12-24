b2c8b680f291bfa4c8ba3842cfabe90f17b06146
Aureliano Yepez <aurelianoyepez@Aurelianos-MacBook.local>
2016-12-23 19:15:48 -0800

    UpdatedCommenst

---
+++ Homework2.rb
@@ -0,0 +1,25 @@
+##Ruby Program Introduction
+
+#Chapter 6 - 8 Finished on 
+#Chapter 6: More About Methods
+#Chapter 7: Flow Control
+#Chapter 8: Arrays and Iterators
+
+##Chapter 6 More About Methods
+##So far we have used methods such as gets, puts, etc.
+##Objects to nouns as to Methods to Verbs
+##10 Methods learned insofar:
+  ## puts, gets, chomp
+  ## .to_i .to_f .to_s
+  ## + - * /
+##Click.tick example -> Click = object, .tick -> method
+  ##Every verb needs a noun  = every method needs an object
+  ##Does not always have to have a dot !
+puts('hello ' .+ 'world') ## .+ is == +
+puts((10.*9).+9) ## .* is == *
+##Works: 'pig'.*5
+##!Works: 5.*'pig'
+puts self ##Prints out the object you are in (main)
+##RECAP: EVERY METHOD HAS AN OBJECT
+
+#FancyStringMethods
