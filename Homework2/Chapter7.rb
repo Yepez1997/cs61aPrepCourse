4c368d4ce171d9c1e9a230bfe4230d0a84e44578
Aureliano Yepez <yepez4845@gmail.com>
2016-12-27 14:25:51 -0800

    Update

--- Homework2/Chapter7.rb
+++ Homework2/Chapter7.rb
@@ -39,7 +39,7 @@ puts 'Hello, ' + name + '.'
 if name == 'Aureliano'
   puts 'What an awesome name'
 end
-##In Simple terms, if the name == Aureliano, the code under the boolean executes
+##In Simple terms, if the name == Aureliano, the code under executes
 ##If the name is != Aureliano  and is == to another string, the code does not execute the if statement
 ##Cleaner code makes debugging easier
   ##Indenting code: nice, clean, and consistent
@@ -74,3 +74,124 @@ else ##If user input is not capitalize, then else statement runs and asks user f
   end ##This end statement ends the second branch
 end ##This end statement end the first branch
 ## Use Good Indentation for easier branch reading
+
+##TIP: WRITE IFS ELSEs AND ENDS TOGETHER, THEN FILL WITH COMMENTS
+##Helps keep track of the code
+##Example
+## if name == name.capitalize
+## else
+##  if
+##  else
+##  end
+## end
+##BookExampleCleanCode
+puts 'Hello, and welcome to tenth grade English.'
+puts 'My name is Mrs. Collopy. And your name is ....?'
+name = gets.chomp
+##Comments before code can almost act as pseudocode
+if name == name.capitalize
+  ## She's civil
+else
+  ## She gets mad
+end
+
+##Looping
+##When you want a computer to do something over and over again
+##While loops: certain codition while true
+##While tests the condition at the top of the loop
+##Input gets replaced by input = gets.chomp and then gets tested again
+input = ' ' ##EmptyString
+while input != 'bye' ##When EmptyString != 'bye' the loop keeps continuing
+  puts input         ##Input is empty and ask for a response, then prints response
+  input = gets.chomp ##Notice that the puts is before gets
+end ##If response = 'bye', the while loop is not true so the code exits and ends to the next line
+puts 'Come again soon !'
+
+##RealRubyProgramBookExample
+while 'Spike' > 'Angel' ##This is always true, lexicographical order
+  input = gets.chomp ##Ask input
+  puts input ##Puts input
+
+  if input == 'bye' ##If input == 'bye', the code breaks
+    break
+  end
+end
+
+puts 'Come again soon !' ##Once the loop ends this line executes
+
+##TIP USE THE ACTUAL TRUE OBJECT IN THE WHILE STATEMENT (I.E WHILE TRUE)
+
+##RealRubyProgramBookExampleWithWhileTrue
+while true  ##While true
+  input = gets.chomp ##Ask input
+  puts input ##Puts input
+
+  if input == 'bye' ##If input == 'bye', the code breaks
+    break
+  end
+end
+
+puts 'Come again soon !' ##Once the loop ends this line executes
+##If code gets stuck in a loop Control-C
+
+##ALittleBitOfLogic
+##ExampleOfUnclearCode
+##CODE Works
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris'
+  puts 'What a lovely name'
+else
+  if name == 'Kathy'
+    puts 'What a lovely name'
+  end
+end
+##ExampleOfClearCode
+##Uses elsif
+##Still unclear beacuse the code repeats itslef
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris'
+  puts 'What a lovely name'
+elsif name == 'Kathy'
+  puts 'What a lovely name'
+end
+##BetterExampleOfClearCode
+##This code is shorter, more concise, and easier to read
+##TIP: BE EXPLICIT WITH YOUR CODE
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris' || name == 'Kathy' ## || = or
+  puts 'What a lovely name'
+end
+## if name == ('Chris'||'Kathy') ##WRONG
+##The computer does not know whether 'Chris' or 'Kathy' is true
+
+##LogicalOperators
+## || = or
+## && = and
+## ! = not
+##How they work ?
+i_am_yepez = true  #Variables set to true or false
+i_am_purple = false
+i_like_music = true
+i_eat_grass = false
+##LogicalOperatorsExamples
+## AND
+puts i_am_yepez && i_like_music   ##true
+puts i_like_music && i_eat_grass  ##false
+puts i_am_purple && i_like_music  ##false
+puts i_am_purple && i_eat_grass   ##false
+puts
+## OR
+puts i_am_yepez || i_like_music   ##true
+puts i_like_music || i_eat_grass  ##true
+puts i_am_purple || i_like_music  ##true
+puts i_am_purple || i_eat_grass   ##false
+puts
+## NOT
+puts !i_am_purple                 ##true
+puts !i_am_yepez                  ##false
